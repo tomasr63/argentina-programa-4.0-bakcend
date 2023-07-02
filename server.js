6b1ab54998781ac920c731374e5c5e629690fdd4
tomasr <lezamatomas@hotmail.com>
2023-07-02 00:27:10 -0300

    Agregando metodos get, put y delete

--- server.js
+++ server.js
@@ -23,17 +23,60 @@ app.use((req, res, next) => {
   next(); // Pasar al siguiente middleware o ruta
 });
 
+
+
 // Ruta principal que devuelve los datos de las frutas
 app.get("/", (req, res) => {
-   res.send(BD);
+  res.send(BD);
+});
+
+// Ruta para ubicar un producto por id
+app.get("/id/:id", (req, res) => {
+  let idParam = parseInt(req.params.id);
+  let respuesta = BD.find((fruta) => fruta.id === idParam);
+
+  respuesta ? res.json(respuesta) : res.json({ Error: `No hay coincidencias para el id: ${idParam}` });
 });
 
 // Ruta para agregar una nueva fruta al arreglo y guardar los cambios
 app.post("/", (req, res) => {
-    const nuevaFruta = req.body;
-    BD.push(nuevaFruta); // Agregar la nueva fruta al arreglo
-    guardarFrutas(BD); // Guardar los cambios en el archivo
-    res.status(201).send("Fruta agregada!"); // Enviar una respuesta exitosa
+  const nuevaFruta = req.body;
+  BD.push(nuevaFruta); // Agregar la nueva fruta al arreglo
+  guardarFrutas(BD); // Guardar los cambios en el archivo
+  res.status(201).send("Fruta agregada!"); // Enviar una respuesta exitosa
+});
+
+// Ruta para modificar un producto
+app.put("/id/:id", (req, res) => {
+  let idParam = parseInt(req.params.id);
+  const frutaModificada = req.body;
+
+  let indexFrutaAModificar = BD.findIndex((fruta) => fruta.id === idParam);
+
+  if (indexFrutaAModificar !== -1) {
+    BD[indexFrutaAModificar] = frutaModificada;
+    guardarFrutas(BD);
+    res.status(200).send("Fruta modificada!");
+  } else {
+    res.status(404).send("Fruta no encontrada.");
+    console.log(indexFrutaAModificar);
+  }
+});
+
+// Ruta para eliminar un producto existente
+app.delete("/id/:id", (req, res) => {
+  let idParam = parseInt(req.params.id);
+
+  let indexFrutaAEliminar = BD.findIndex((fruta) => fruta.id === idParam);
+
+  if (indexFrutaAEliminar !== -1) {
+    BD.splice(indexFrutaAEliminar, 1);
+    guardarFrutas(BD);
+    res.status(200).send("Fruta Eliminada!");
+  } else {
+    res.status(404).send("Fruta no encontrada.");
+    console.log(indexFrutaAModificar);
+  }
 });
 
 // Ruta para manejar las solicitudes a rutas no existentes
@@ -43,5 +86,5 @@ app.get("*", (req, res) => {
 
 // Iniciar el servidor y escuchar en el puerto especificado
 app.listen(PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${PORT}`);
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
